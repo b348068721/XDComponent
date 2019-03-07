ffb515ea1dd7d872f32c65d9b7852769274bb873
b348068721 <guanxidonghrb14@163.com>
2019-03-07 16:12:27 +0800

    XDComponent

---
+++ XDComponent/Classes/XDTest.h
@@ -0,0 +1,17 @@
+//
+//  XDTest.h
+//  XDComponent_Example
+//
+//  Created by winter on 2019/3/7.
+//  Copyright © 2019 b348068721. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XDTest : NSObject
+- (void)setTestLog;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XDComponent/Classes/XDTest.m
@@ -0,0 +1,15 @@
+//
+//  XDTest.m
+//  XDComponent_Example
+//
+//  Created by winter on 2019/3/7.
+//  Copyright © 2019 b348068721. All rights reserved.
+//
+
+#import "XDTest.h"
+
+@implementation XDTest
+- (void)setTestLog {
+    NSLog(@"log");
+}
+@end
