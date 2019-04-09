8aed56b85392ac4878e4cd98cb95063d188ce6fe
b348068721 <guanxidonghrb14@163.com>
2019-04-09 15:44:10 +0800

    init project

---
+++ XDComponent/Classes/BaseViewController.h
@@ -0,0 +1,16 @@
+//
+//  BaseViewController.h
+//  Pods-XDComponent_Example
+//
+//  Created by winter on 2019/4/9.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BaseViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
